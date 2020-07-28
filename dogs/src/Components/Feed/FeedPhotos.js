dafc2f2e093101d6e9d1de1216f830b8e1709d4d
andregallacci <andre.gallacci@gmail.com>
2020-07-28 17:18:47 -0300

    =Project almost finished, missing optimization

---
+++ dogs/src/Components/Feed/FeedPhotos.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import FeedPhotosItem from './FeedPhotosItem';
+import useFetch from '../../Hooks/useFetch';
+import { PHOTOS_GET } from '../../Api';
+import Error from '../Helper/Error';
+import Loading from '../Helper/Loading';
+import styles from './FeedPhotos.module.css';
+
+const FeedPhotos = ({ page, user, setModalPhoto, setInfinite }) => {
+  const { data, loading, error, request } = useFetch();
+
+  React.useEffect(() => {
+    async function fetchPhotos() {
+      const total = 9;
+      const { url, options } = PHOTOS_GET({
+        page: page,
+        total: total,
+        user: user,
+      });
+      const { response, json } = await request(url, options);
+      if (response && response.ok && json.length < total) {
+        setInfinite(false);
+      }
+    }
+
+    fetchPhotos();
+  }, [page, request, setInfinite, user]);
+
+  if (error) return <Error />;
+  if (loading) return <Loading />;
+  if (data)
+    return (
+      <ul className={`${styles.feed} animeLeft`}>
+        {data.map((photo) => (
+          <FeedPhotosItem
+            key={photo.id}
+            photo={photo}
+            setModalPhoto={setModalPhoto}
+          />
+        ))}
+      </ul>
+    );
+  else return null;
+};
+
+export default FeedPhotos;
